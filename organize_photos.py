882e0a407cace517fb3f9bbf5de019754b937d51
Slaton <slsp8752@colorado.edu>
2015-08-04 12:33:19 -0600

    finished timestamp parsing and comparing functions

--- organize_photos.py
+++ organize_photos.py
@@ -8,9 +8,32 @@ from tkFileDialog import askdirectory
 
 def get_date_taken(path):
 	return Image.open(path)._getexif()[36867]
+	
+def split_timestamp(stamp):
+	stamp_split = [str(item.strip()) for item in stamp.split(':')]
+	day_hour_split = stamp_split[2].split(' ')
+	day = day_hour_split[0]
+	hour= day_hour_split[1]
+	year = stamp_split[0]
+	month = stamp_split[1]
+	minute = stamp_split[3]
+	second = stamp_split[4]
+	split_list = [year, month, day, hour, minute, second]
+	return split_list
+	
+def is_timestamp_greater(stamp_one, stamp_two):
+	time_one_list = split_timestamp(stamp_one)
+	time_two_list = split_timestamp(stamp_two)
+	for index, entry in enumerate(time_one_list):
+		print entry
+		print time_two_list[index]
+		if entry > time_two_list[index]:
+			return 1
 
 Tk().withdraw() 
+print("Location to put email folders")
 directory = askdirectory() # get location to put email folders
+print("Location of emails.txt")
 emailsfull = [line.rstrip('\n') for line in open(askopenfilename())] # emails and timestamps
 emails = emailsfull[0::2]
 times = emailsfull[1::2]
@@ -21,11 +44,10 @@ for index, address in enumerate(emails):
 	subdirectory = os.path.join(directory, address)
 	if not os.path.exists(subdirectory):
 		os.makedirs(subdirectory)
-	
-print get_date_taken(askopenfilename())
 
 for address, time in stamps.iteritems():
-	print time
+	
+	
 	# parse time
 	# compare to every photo taken that hasn't been sorted (?)
 	# 
